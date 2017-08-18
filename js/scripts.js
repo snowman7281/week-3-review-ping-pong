697360e6cb0535f153b4d56d50aea7ab82a54349
nv <nv781@yahoo.com>
2017-08-18 11:17:13 -0700

    add functionality of game

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,52 @@
 //Business Logic(Back-End)
-var swaps = ["ping", "pong", "ping-pong"];
+function pingPong(numberInput){
+  var numArray = [];
 
-//check to make sure its a number
+  if(numberInput < 0){
+    for(var i = -1; i >= numberInput; i--){
+      numArray.push(index);
+    }
+    numArray.forEach(function(num){
+      if(num % 15 === 0){
+        numArray[-num-1] = "pingpong";
+      }else if(num % 3 === 0){
+        numArray[-num-1] = "ping";
+      }else if(num % 5 === 0){
+        numArray[-number-1] = "pong";
+      }
+    });
+  }else if(numberInput > 0){
+    for(var i = 1; i <= numberInput; i++){
+      numArray.push(i);
+    }
+    numArray.forEach(function(num){
+      if(num % 15 === 0){
+        numArray[num-1] = "pingpong";
+      }else if(num % 3 === 0){
+        numArray[num-1] = "ping";
+      }else if(num % 5 === 0){
+        numArray[num-1] = "pong"
+      }
+    });
+  }
+  return numArray;
+};
 
+function addLineDisplay(arrayOut) {
+    $('ul#result').empty();
+  arrayOut.forEach(function(item) {
+    $('ul#output').append('<li>' + item + '</li>');
+  });
+  return;
+}
 
 
 //UI Userinterface(Front-End)
 $(document).ready(function(){
   $("#ping-pong-form").submit(function(event){
+    var numberInput = $("input#num").val();
+
+    $("#output").text(pingPong(numberInput));
     event.preventDefault();
-    var numInput = parseInt($("#num").val());
-    $("#output").text();
   });
 });
